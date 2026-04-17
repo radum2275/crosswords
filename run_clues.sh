fc81e4e8def4e93960704357c3d56046f39a7f45
Radu Marinescu <radu.marinescu@gmail.com>
2026-04-17 10:56:22 +0000

    Removed the partial run for v4

--- run_clues.sh
+++ run_clues.sh
@@ -8,22 +8,22 @@ p=$2
 
 if [[ "$p" == "v1" ]]; then
     l='log_clues_'${m}'_'${p}'_s0.txt'
-    ./timeout -m 30000000 python src/crosswords/clues_ro.py --model_id $m --dataset_file /home/radu/storage/git/crosswords/data/extracted_data.json --output_dir /home/radu/storage/git/crosswords/data/results --dataset_type clues --version $p --output_name clues --batch_size 200 >& $l
+    ./timeout -m 30000000 python src/crosswords/clues_ro.py --model_id $m --dataset_file /home/radu/storage/git/crosswords/data/extracted_data.json --output_dir /home/radu/storage/git/crosswords/data/results --dataset_type clues --version $p --output_name clues --batch_size 1024 >& $l
 fi
 if [[ "$p" == "v2" ]]; then
     l='log_clues_'${m}'_'${p}'_s0.txt'
-    ./timeout -m 30000000 python src/crosswords/clues_ro.py --model_id $m --dataset_file /home/radu/storage/git/crosswords/data/extracted_data.json --output_dir /home/radu/storage/git/crosswords/data/results --dataset_type clues --version $p --output_name clues --batch_size 200 >& $l
+    ./timeout -m 30000000 python src/crosswords/clues_ro.py --model_id $m --dataset_file /home/radu/storage/git/crosswords/data/extracted_data.json --output_dir /home/radu/storage/git/crosswords/data/results --dataset_type clues --version $p --output_name clues --batch_size 1024 >& $l
 fi
 if [[ "$p" == "v3" ]]; then
     for s in 0 1 2 3 4; do
         l='log_clues_'${m}'_'${p}'_s'${s}'.txt'
-        ./timeout -m 30000000 python src/crosswords/clues_ro.py --model_id $m --dataset_file /home/radu/storage/git/crosswords/data/extracted_data.json --output_dir /home/radu/storage/git/crosswords/data/results --dataset_type clues --version $p --prefix_len $s --output_name clues --batch_size 200 >& $l
+        ./timeout -m 30000000 python src/crosswords/clues_ro.py --model_id $m --dataset_file /home/radu/storage/git/crosswords/data/extracted_data.json --output_dir /home/radu/storage/git/crosswords/data/results --dataset_type clues --version $p --prefix_len $s --output_name clues --batch_size 1024 >& $l
     done
 fi
 if [[ "$p" == "v4" ]]; then
     for s in 0 1 2 3 4; do
         l='log_clues_'${m}'_'${p}'_s'${s}'.txt'
-        ./timeout -m 30000000 python src/crosswords/clues_ro.py --model_id $m --dataset_file /home/radu/storage/git/crosswords/data/extracted_data.json --output_dir /home/radu/storage/git/crosswords/data/results --dataset_type clues --version $p --prefix_len $s --output_name clues --batch_size 200 >& $l
+        ./timeout -m 30000000 python src/crosswords/clues_ro.py --model_id $m --dataset_file /home/radu/storage/git/crosswords/data/extracted_data.json --output_dir /home/radu/storage/git/crosswords/data/results --dataset_type clues --version $p --prefix_len $s --output_name clues --batch_size 1024 >& $l
     done
 fi
 
